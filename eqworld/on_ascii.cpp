758ae9a0326d0eb578506f25b81fd5f711eb9a8a
Grandpa Trula <grandpat@un-g1.kzn.21-school.ru>
2023-02-01 18:56:04 +0300

    cpp contaner, waiting

---
+++ eqworld/on_ascii.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+ using namespace std;
+
+int main () {
+    string a = "Hello", b = "zas";
+    if (a > b) {
+        cout << 'a';
+    } else {
+        cout << 'b';
+    }
+}
